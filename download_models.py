575dcd1f8caf79ab73568404b70548546b3b66c5
Atul Deshpande <deshpandeatul05@gmail.com>
2025-11-17 17:53:01 +0530

    cross encoder

--- download_models.py
+++ download_models.py
@@ -6,9 +6,11 @@ model_name = "microsoft/Phi-3-mini-4k-instruct"
 
 save_dir = "./models/phi_mini"
 embd_dir = "./models/embeddings"
+cross_enc_dir = "./models/cross_enc"
 
 os.makedirs(save_dir, exist_ok=True)
 os.makedirs(embd_dir, exist_ok=True)
+os.makedirs(cross_enc_dir, exist_ok=True)
 print(f"Folders created!!!\n")
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=False)
@@ -25,3 +27,9 @@ embedder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 embedder.save(embd_dir)
 
 print(f"Saved model at {embd_dir}")
+
+cross_encoder = SentenceTransformer("cross-encoder/ms-marco-MiniLM-L-6-v2")
+cross_encoder.save(cross_enc_dir)
+
+print(f"Saved model at {cross_encoder}")
+
